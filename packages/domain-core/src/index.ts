7f86cdceb7c9e8eba32e280f16f4380b2ebd249e
Mostafa Gad <magad84@gmail.com>
2026-08-05 22:15:14 +0400

    feat(engine): export integrated decision evaluator

--- packages/domain-core/src/index.ts
+++ packages/domain-core/src/index.ts
@@ -2,3 +2,4 @@ export * from "./cargo-calculator.js";
 export * from "./trade-lane-evaluator.js";
 export * from "./country-requirements-evaluator.js";
 export * from "./dg-prescreen.js";
+export * from "./integrated-decision-engine.js";
